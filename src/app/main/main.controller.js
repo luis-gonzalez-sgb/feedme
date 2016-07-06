d0cb583c36d92dd34164e5d991e23be50ef0e13a
Luis González <luis.gonzalez@synergy-gb.com>
2016-07-06 09:42:30 -0400

    A few changes.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,34 +6,41 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
+  MainController.$inject= [
+    "$timeout",
+    "webDevTec",
+    "toastr"
+  ]
+
   function MainController($timeout, webDevTec, toastr) {
     var vm = this;
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
-    vm.creationDate = 1467807332960;
-    vm.showToastr = showToastr;
-
-    activate();
+    vm.creationDate = 1467807332960;    
 
     function activate() {
-      getWebDevTec();
+      vm.getWebDevTec();
       $timeout(function() {
         vm.classAnimation = 'rubberBand';
       }, 4000);
     }
 
-    function showToastr() {
+    vm.showToastr = function() {
       toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
       vm.classAnimation = '';
     }
 
-    function getWebDevTec() {
+    vm.getWebDevTec = function() {
       vm.awesomeThings = webDevTec.getTec();
 
       angular.forEach(vm.awesomeThings, function(awesomeThing) {
         awesomeThing.rank = Math.random();
       });
     }
+
+    activate();
+
   }
+
 })();
